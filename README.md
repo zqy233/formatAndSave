e53d41ebe7d901a69a5c46cccfb2a80bc8dade30
zqylzcwcxy <347891134@qq.com>
2022-04-20 12:03:57 +0800

    :zap:improve performance : 放弃自动关闭分栏功能，以得到更好的体验

--- README.md
+++ README.md
@@ -24,23 +24,37 @@ volar分栏写代码的效果真的很爽，会自动拆分script标签，templa
 
 ### 🔖 使用
 
-实现了类似的分栏效果，本插件供一个`copy editor and auto fold`命令，默认快捷键`ctrl+j`,`ctrl+j`后会打开分栏，再次`ctrl+j`则会关闭分栏，如果跟你的快捷键冲突，你也可以在工具-自定义快捷键中更改该快捷键
+实现了类似的分栏效果，本插件供一个`copy editor and auto fold`命令，默认快捷键`ctrl+j`,`ctrl+j`后会在当前vue文件右侧打开分栏
+
+默认左侧折叠script标签，显示template和style标签，右侧分栏则折叠template和style标签，显示script标签（即当前vue文件需要script标，template和style标签才有效果）
+
+工具>配置>插件配置中可以勾选本插件分栏的模式
+
+取消勾选后分栏效果跟默认的相反，即左侧显示script标签，折叠template和style标签，右侧分栏则显示template和style标签，折叠script标签
+
+建议自行尝试一下，来决定喜欢的分栏模式
+
+### 🔖 关闭分栏
+
+本插件不提供关闭分栏的快捷键，如想关闭分栏，可以鼠标点击叉号或使用`关闭当前标签`，`关闭当前所有标签`等等HBuilderX内置快捷键
+
+### 🔖 更改该快捷键
+
+如果跟你的快捷键冲突，你也可以在工具-自定义快捷键中更改该快捷键
 
 ```json
  {"key":"ctrl+k","command":"extension.copyEditorAutoFold","override":true }
 ```
 
-### 🔖 分栏模式
+### 🚨 遗憾
 
-工具>配置>插件配置中可以勾选本插件分栏的模式
+原本想实现`ctrl+j`打开分栏，再次`ctrl+j`则会关闭分栏这样一个效果
 
-默认勾选,左侧折叠script标签，显示template和style标签，右侧分栏则折叠template和style标签，显示script标签
+分别尝试了HBuilderX内置的命令`关闭当前标签`，`单栏`命令来实现这一功能
 
-取消勾选后分栏效果跟默认的相反，建议自行尝试一下，来决定喜欢的分栏模式
+使用`关闭当前标签`命令，因为没有监听关闭标签的HBuilderX的api，很容易在使用过程中产生各类问题
 
-### 🚨 注意，`copy editor and auto fold`有关闭当前页面的效果
+使用`单栏`命令，可以实现再次`ctrl+j`则会关闭分栏这样一个效果，但是HBuilderX使用`单栏`命令关闭多栏，相同的标签会重复出现，比如双栏情况左分栏有index.vue,main.js,右分栏也有相同的index.vue,main.js，使用`单栏`命令后，会出现index.vue,main.js,index.vue,main.js总共四个标签。所以如果左右分栏有很多相同的标签，变成单栏后会有很多重复的标签，最终的体验会很差，我认为，相同的标签应当在变成单栏后自动合并
 
-关闭分栏的功能是通过关闭当前页面实现的，所以当前页面没有分栏则会直接关闭当前页面
-
-当然，这一般也不会有什么问题，关闭当前页面前HBuilderX都会提醒你进行保存的
+所以最终放弃再次`ctrl+j`则会关闭分栏这个功能，期待HBuilderX未来提供更多功能的api吧
 
--- extension.js
+++ extension.js
@@ -2,36 +2,33 @@ const hx = require("hbuilderx")
 
 //该方法将在插件激活的时候调用
 function activate(context) {
+  // 一键格式化和保存
   let formatAndSave = hx.commands.registerCommand("extension.formatAndSave", async () => {
     await hx.commands.executeCommand("editor.action.format")
     hx.commands.executeCommand("workbench.action.files.save")
   })
-  let num = 0
-  let copyEditorAutoFold = hx.commands.registerCommand("extension.copyEditorAutoFold", async () => {
-    num++
-    // 偶数次数表示已打开分栏，进行关闭
-    if (num % 2 == 0) {
-      await hx.commands.executeCommand("workbench.action.closeActiveEditor")
-      return hx.commands.executeCommand("workbench.action.foldAllExpand")
-    }
-    // 奇数次数表示打开分栏
+  // 双分栏且自动折叠
+  let copyEditorAutoFold = hx.commands.registerCommand("extension.copyEditorAutoFold", async() => {
     let config = hx.workspace.getConfiguration("formatAndSave")
     const mode = config.get("mode")
     let activeEditor = await hx.window.getActiveTextEditor()
+
     const word = activeEditor.document.getText()
     const scriptIndex = word.indexOf("<script")
     const templateIndex = word.indexOf("<template")
     const styleIndex = word.indexOf("<style")
     if (mode) {
-      // 折叠script
+      // 左侧折叠script
       activeEditor.setSelection(scriptIndex, scriptIndex)
       await hx.commands.executeCommand("workbench.action.foldChildrenContract")
       //  复制分栏，会复制左侧光标位置
       await hx.commands.executeCommand("workbench.action.copyEditor")
       if (scriptIndex == -1) return
+      // 右侧折叠非script
       await hx.commands.executeCommand("workbench.action.foldAllContract")
       await hx.commands.executeCommand("workbench.action.foldChildrenExpand")
     } else {
+      // 左侧折叠非script
       activeEditor.setSelection(templateIndex, templateIndex)
       await hx.commands.executeCommand("workbench.action.foldChildrenContract")
       activeEditor.setSelection(styleIndex, styleIndex)
@@ -40,6 +37,7 @@ function activate(context) {
       // 复制分栏，会复制左侧光标位置
       await hx.commands.executeCommand("workbench.action.copyEditor")
       if (scriptIndex == -1) return
+      // 右侧折叠script
       await hx.commands.executeCommand("workbench.action.foldChildrenContract")
     }
   })
