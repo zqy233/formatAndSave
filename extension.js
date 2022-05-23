da5f84df157936d467d2a2fb2f59c7306ce13a90
zqylzcwcxy <347891134@qq.com>
2022-05-23 23:18:41 +0800

    :sparkles:new feature : 恢复一键格式化保存命令

--- extension.js
+++ extension.js
@@ -1,8 +1,10 @@
 const copyEditorAutoFold = require("./copyEditorAutoFold.js")
 const jumpQuote = require("./jumpQuote.js")
+const formatAndSave = require("./formatAndSave.js")
 //该方法将在插件激活的时候调用
 function activate(context) {
   //订阅销毁钩子，插件禁用的时候，自动注销该command
+  context.subscriptions.push(formatAndSave)
   context.subscriptions.push(copyEditorAutoFold)
   context.subscriptions.push(jumpQuote.jumpLastQuote)
   context.subscriptions.push(jumpQuote.jumpNextQuote)
