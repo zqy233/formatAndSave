138524abb0311ecaaac7423d607895e0c66a8b1e
zqylzcwcxy <347891134@qq.com>
2022-10-06 18:21:32 +0800

    :bug:fix bug : 修复单击情况下注释不正确的问题

--- src/powerfulComment.js
+++ src/powerfulComment.js
@@ -9,25 +9,27 @@ const powerfulComment = hx.commands.registerCommand("extension.powerfulComment",
   selections.forEach(async selection => {
     await comment(selection, document, activeEditor)
   })
-  console.log("selections", selections);
 })
 
 module.exports = powerfulComment
 
+/** 二次封装注释函数 */
 function comment(selection, document, activeEditor) {
   return new Promise(async resolve => {
     let {
-      start,
-      end
+      start: oldStart,
+      end: oldEnd,
     } = selection
+
     // 获取开始行结束行的信息，计算上\t\n符号的位置，获取正确的注释位置
-    const startAndEndLine = await Promise.all([document.lineFromPosition(start), document.lineFromPosition(end)])
+    const startAndEndLine = await Promise.all([document.lineFromPosition(oldStart), document.lineFromPosition(
+      oldEnd)])
     const matchReg1 = startAndEndLine[0].text.match(/^\s*/g)
     const matchReg2 = startAndEndLine[1].text.match(/\s*$/g)
-    // 开始位置=加上行开始位置+\t\n
-    start = startAndEndLine[0].start + matchReg1[0].length
+    // 开始位置=行开始位置+\t\n
+    const start = startAndEndLine[0].start + matchReg1[0].length
     // 结束位置=行结束位置-\t\n
-    end = startAndEndLine[1].end - matchReg2[0].length
+    const end = startAndEndLine[1].end - matchReg2[0].length
 
     // 获取选中文本内容
     let word = document.getText({
@@ -40,11 +42,10 @@ function comment(selection, document, activeEditor) {
 
     // 获取选择文本中所有`/*`和`*/`的位置
     const styleCommentPositions = getCommentPositions(word, start, "/*", "*/")
-    console.log("sssss", commentPositions, styleCommentPositions);
 
-    // 没有<!--和-->注释的情况
+    // 选中内容没有注释，直接调用注释命令
     if (!commentPositions.length && !styleCommentPositions.length) {
-      return noCommentPositionsDeal(activeEditor, start, end)
+      return noCommentPositionsDeal(activeEditor, start, end, oldStart, oldEnd)
     }
 
     // 注释是`<!--`和`-->`
@@ -53,7 +54,7 @@ function comment(selection, document, activeEditor) {
       const betweenCommentPositions = getBetweenCommentPositions(commentPositions, start, end, word)
 
       // 如果存在非注释区域，就注释这些区域，如果不存在，那就取消所有注释
-      commentPositionsDeal(betweenCommentPositions, commentPositions, activeEditor, start, end)
+      commentPositionsDeal(betweenCommentPositions, commentPositions, activeEditor, oldStart, oldEnd)
     }
 
     // 注释是`/*`和`*/`
@@ -61,21 +62,21 @@ function comment(selection, document, activeEditor) {
       // 有/*和*/注释的情况，获取非注释区域的位置
       const betweenCommentPositions = getBetweenCommentPositions(styleCommentPositions, start, end, word)
       // 如果存在非注释区域，就注释这些区域，如果不存在，那就取消所有注释
-      commentPositionsDeal(betweenCommentPositions, styleCommentPositions, activeEditor, start, end)
+      commentPositionsDeal(betweenCommentPositions, styleCommentPositions, activeEditor, oldStart, oldEnd)
     }
     resolve()
   })
 }
 
-/** 获取选择文本中所有`<!--`和`-->`的位置 */
+/** 获取选择文本中所有指定注释的位置 */
 function getCommentPositions(word, start, startComment, endComment) {
   const commentPositions = []
   let startPosition = word.indexOf(startComment)
   let endPosition = word.indexOf(endComment)
-  while (startPosition > -1 || endPosition > -1) {
+  while (startPosition > -1 && endPosition > -1) {
     commentPositions.push({
-      start: startPosition !== -1 ? start + startPosition : null,
-      end: endPosition !== -1 ? start + endPosition + endComment.length : null,
+      start: start + startPosition,
+      end: start + endPosition + endComment.length,
     })
     startPosition = word.indexOf(startComment, startPosition + startComment.length)
     endPosition = word.indexOf(endComment, endPosition + endComment.length)
@@ -83,19 +84,19 @@ function getCommentPositions(word, start, startComment, endComment) {
   return commentPositions
 }
 
-/** 没有<!--和-->注释的情况 */
-async function noCommentPositionsDeal(activeEditor, start, end) {
+/** 选中内容没有注释，直接调用注释命令 */
+async function noCommentPositionsDeal(activeEditor, start, end, oldStart, oldEnd) {
   activeEditor.setSelection(start, end)
-  hx.commands.executeCommand("editor.action.commentLine")
+  await hx.commands.executeCommand("editor.action.commentLine")
+  activeEditor.setSelection(oldStart, oldEnd)
 }
 
-/** 有<!--和-->注释的情况，获取非注释区域的位置 */
+/** 有注释的情况，获取非注释区域的位置 */
 function getBetweenCommentPositions(commentPositions, start, end, word) {
   const betweenCommentPositions = []
   for (let i = 0; i < commentPositions.length; i++) {
-    const vaild = commentPositions[i].start && commentPositions[i].end
     // 选中起始位置到第一个<!--的位置
-    if (i === 0 && vaild && commentPositions[i].start > start) {
+    if (i === 0 && commentPositions[i].start > start) {
       const matchReg = word.slice(0, commentPositions[i].start - start).match(/^\s*|\s*$/g)
       if (matchReg.length === 1) continue
       const newStart = start + matchReg[0].length // 选中文本起始位置+算上\t\n后的位置
@@ -108,11 +109,10 @@ function getBetweenCommentPositions(commentPositions, start, end, word) {
       }
     }
     // 中间非注释区域
-    if (i + 1 < commentPositions.length && vaild) {
+    if (i + 1 < commentPositions.length) {
       const matchReg = word.slice(commentPositions[i].end - start, commentPositions[i + 1].start - start).match(
         /^\s*|\s*$/g)
       if (matchReg.length === 1) continue
-      console.log(matchReg)
       const newStart = commentPositions[i].end + matchReg[0].length
       const newEnd = commentPositions[i + 1].start - matchReg[1].length
       if (newStart !== newEnd) {
@@ -123,7 +123,7 @@ function getBetweenCommentPositions(commentPositions, start, end, word) {
       }
     }
     //  选中结束位置到最后一个-->的位置
-    if (i === commentPositions.length - 1 && vaild && commentPositions[i].end !== end) {
+    if (i === commentPositions.length - 1 && commentPositions[i].end !== end) {
       const matchReg = word.slice(commentPositions[i].end - start, end - start).match(/^\s*|\s*$/g)
       if (matchReg.length === 1) continue
       const newStart = commentPositions[i].end + matchReg[0].length
@@ -143,16 +143,16 @@ function getBetweenCommentPositions(commentPositions, start, end, word) {
 async function commentPositionsDeal(betweenCommentPositions, commentPositions, activeEditor, start, end) {
   // 存在非注释区域，选中所有非注释区域
   if (betweenCommentPositions.length) {
-    activeEditor.setSelection(betweenCommentPositions[0].start, betweenCommentPositions[0].end) // 设置初始光标位置
+    activeEditor.setSelection(betweenCommentPositions[0].start, betweenCommentPositions[0].end)
     for (let i = 1; i < betweenCommentPositions.length; i++) {
-      activeEditor.addSelection(betweenCommentPositions[i].start, betweenCommentPositions[i].end) // 设置其余光标位置
+      activeEditor.addSelection(betweenCommentPositions[i].start, betweenCommentPositions[i].end)
     }
   }
   // 不存在非注释区域，选中所有注释区域
   else {
-    activeEditor.setSelection(commentPositions[0].start, commentPositions[0].end) // 设置初始光标位置
+    activeEditor.setSelection(commentPositions[0].start, commentPositions[0].end)
     for (let i = 1; i < commentPositions.length; i++) {
-      activeEditor.addSelection(commentPositions[i].start, commentPositions[i].end) // 设置其余光标位置
+      activeEditor.addSelection(commentPositions[i].start, commentPositions[i].end)
     }
   }
   await hx.commands.executeCommand("editor.action.commentLine") // 调用注释命令
