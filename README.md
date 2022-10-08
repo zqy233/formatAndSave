b451f62976d31c6105d694925f38620efb12faef
zqylzcwcxy <347891134@qq.com>
2022-10-08 10:20:27 +0800

    :sparkles:new feature : 开始行和结束行不用完整选择

--- README.md
+++ README.md
@@ -14,11 +14,39 @@
 
 > 二次封装注释命令，快捷键仍为`ctrl+/`
 
-以往，如果选中区域中存在部分注释，这时直接注释会出现错误，只能依次选中没有注释的地方，进行注释
+支持html和css（包括vue）嵌套注释下进行注释，它会自动判断所有注释区域，然后分段进行相应注释，解开注释也会进行判断，再也不用一个个选择解开，直接全选多个注释，一键后会进行分段解开注释
 
-而封装后的注释命令，支持html和css（包括vue）嵌套注释下进行注释（暂不支持scss、less的嵌套），它会自动判断所有注释区域，然后分段进行相应注释，解开注释也会进行判断，再也不用一个个选择解开，直接全选多个注释，一键后会进行分段解开注释
+例如，选择如下内容
 
-fix：顺带修复了，HbuilderX注释命令开始行和结束行如果不选择完全，注释的位置会出现错误的问题
+```html
+<scroll-view scroll-x="true">
+			<view class="scroll-inner">
+				<!-- <image v-for="item in imgList" :key="item.id" :src="item.imageUrl" mode="aspectFill"></image> -->
+			</view>
+		</scroll-view>
+```
+
+这时直接注释会出现错误，会变成这样，只能依次选中没有注释的地方，依次注释
+
+```html
+<!-- <scroll-view scroll-x="true">
+			<view class="scroll-inner">
+				<!-- <image v-for="item in imgList" :key="item.id" :src="item.imageUrl" mode="aspectFill"></image> -->
+			</view>
+		</scroll-view> -->
+```
+
+而封装后的注释命令，会自动进行分段注释，如果想解开这三个注释，直接选中整个区域，一键后即可全部解开
+
+```html
+	<!-- <scroll-view scroll-x="true">
+			<view class="scroll-inner"> -->
+				<!-- <image v-for="item in imgList" :key="item.id" :src="item.imageUrl" mode="aspectFill"></image> -->
+			<!-- </view>
+		</scroll-view> -->
+```
+
+fix：顺带修复了，HBuilderX注释命令开始行和结束行如果不选择完全，注释的位置会出现错误的问题
 
 ##  ✨功能 3 快速选中双引号区域
 
--- src/powerfulComment.js
+++ src/powerfulComment.js
@@ -5,7 +5,6 @@ const powerfulComment = hx.commands.registerCommand("extension.powerfulComment",
     selections,
     document
   } = activeEditor
-
   selections.forEach(async selection => {
     await comment(selection, document, activeEditor)
   })
@@ -17,19 +16,25 @@ module.exports = powerfulComment
 function comment(selection, document, activeEditor) {
   return new Promise(async resolve => {
     let {
-      start: oldStart,
-      end: oldEnd,
+      start: originStart,
+      end: originEnd
     } = selection
 
     // 获取开始行结束行的信息，计算上\t\n符号的位置，获取正确的注释位置
-    const startAndEndLine = await Promise.all([document.lineFromPosition(oldStart), document.lineFromPosition(
-      oldEnd)])
+    const startAndEndLine = await Promise.all([document.lineFromPosition(originStart), document.lineFromPosition(
+      originEnd)])
     const matchReg1 = startAndEndLine[0].text.match(/^\s*/g)
     const matchReg2 = startAndEndLine[1].text.match(/\s*$/g)
     // 开始位置=行开始位置+\t\n
     const start = startAndEndLine[0].start + matchReg1[0].length
     // 结束位置=行结束位置-\t\n
     const end = startAndEndLine[1].end - matchReg2[0].length
+
+    // 单击，则直接调用命令
+    if (originStart === originEnd) {
+      noCommentPositionsDeal(activeEditor, start, end, originStart, originEnd)
+      return resolve()
+    }
 
     // 获取选中文本内容
     let word = document.getText({
@@ -38,23 +43,24 @@ function comment(selection, document, activeEditor) {
     })
 
     // 获取选择文本中所有`<!--`和`-->`的位置
-    const commentPositions = getCommentPositions(word, start, "<!--", "-->")
+    const commentPositions = getCommentPositions(word, start, end, "<!--", "-->")
 
     // 获取选择文本中所有`/*`和`*/`的位置
-    const styleCommentPositions = getCommentPositions(word, start, "/*", "*/")
+    const styleCommentPositions = getCommentPositions(word, start, end, "/*", "*/")
 
     // 选中内容没有注释，直接调用注释命令
     if (!commentPositions.length && !styleCommentPositions.length) {
-      return noCommentPositionsDeal(activeEditor, start, end, oldStart, oldEnd)
+      noCommentPositionsDeal(activeEditor, start, end, originStart, originEnd)
+      return resolve()
     }
 
     // 注释是`<!--`和`-->`
     if (commentPositions.length) {
       // 有<!--和-->注释的情况，获取非注释区域的位置
       const betweenCommentPositions = getBetweenCommentPositions(commentPositions, start, end, word)
-
       // 如果存在非注释区域，就注释这些区域，如果不存在，那就取消所有注释
-      commentPositionsDeal(betweenCommentPositions, commentPositions, activeEditor, oldStart, oldEnd)
+      commentPositionsDeal(betweenCommentPositions, commentPositions, activeEditor, originStart, originEnd)
+      return resolve()
     }
 
     // 注释是`/*`和`*/`
@@ -62,33 +68,68 @@ function comment(selection, document, activeEditor) {
       // 有/*和*/注释的情况，获取非注释区域的位置
       const betweenCommentPositions = getBetweenCommentPositions(styleCommentPositions, start, end, word)
       // 如果存在非注释区域，就注释这些区域，如果不存在，那就取消所有注释
-      commentPositionsDeal(betweenCommentPositions, styleCommentPositions, activeEditor, oldStart, oldEnd)
+      commentPositionsDeal(betweenCommentPositions, styleCommentPositions, activeEditor, originStart, originEnd)
+      return resolve()
     }
-    resolve()
   })
 }
 
 /** 获取选择文本中所有指定注释的位置 */
-function getCommentPositions(word, start, startComment, endComment) {
+function getCommentPositions(word, start, end, startComment, endComment) {
   const commentPositions = []
   let startPosition = word.indexOf(startComment)
   let endPosition = word.indexOf(endComment)
-  while (startPosition > -1 && endPosition > -1) {
+
+  // 情景: 选择文本中起始部分注释不完整，只有-->部分，没有<!--部分，例如：
+  // ></image> -->
+  //     <image src="../../static/img/recommend1.jpeg" mode="heightFix" />
+  if (startPosition === -1 && endPosition > -1) {
     commentPositions.push({
-      start: start + startPosition,
+      start: start,
       end: start + endPosition + endComment.length,
     })
+  }
+  while (startPosition > -1 && endPosition > -1) {
+    // 情景: 选择文本中注释有完整有残缺，例如：
+    // ></image> -->
+    // <!-- <image src="../../static/img/recommend1.jpeg" mode="heightFix" /> -->
+    // <!-- <image src="../../static/img/recommend2.jpg" mode="heightFix" />
+    // 残缺部分不用考虑，只需要完整的正确位置，如果startPosition大于endPosition，说明不是同一对，调整startPosition的位置
+    if (startPosition > endPosition) {
+      startPosition = endPosition + endComment.length
+      commentPositions.push({
+        start: start,
+        end: start + endPosition + endComment.length,
+      })
+    }
+    // 情景:注释完整
+    if (startPosition < endPosition) {
+      commentPositions.push({
+        start: start + startPosition,
+        end: start + endPosition + endComment.length,
+      })
+    }
     startPosition = word.indexOf(startComment, startPosition + startComment.length)
     endPosition = word.indexOf(endComment, endPosition + endComment.length)
+  }
+
+  // 情景:  选择文本中末尾注释不完整，只有<!--部分，没有-->部分，例如：
+  // <view class="scroll-inner">
+  // <!-- <image
+  if (startPosition > -1 && endPosition === -1) {
+    commentPositions.push({
+      start: start + startPosition,
+      end: end,
+    })
   }
   return commentPositions
 }
 
 /** 选中内容没有注释，直接调用注释命令 */
-async function noCommentPositionsDeal(activeEditor, start, end, oldStart, oldEnd) {
+async function noCommentPositionsDeal(activeEditor, start, end, originStart, originEnd) {
   activeEditor.setSelection(start, end)
   await hx.commands.executeCommand("editor.action.commentLine")
-  activeEditor.setSelection(oldStart, oldEnd)
+  activeEditor.setSelection(originStart, originEnd)
 }
 
 /** 有注释的情况，获取非注释区域的位置 */
@@ -98,7 +139,6 @@ function getBetweenCommentPositions(commentPositions, start, end, word) {
     // 选中起始位置到第一个<!--的位置
     if (i === 0 && commentPositions[i].start > start) {
       const matchReg = word.slice(0, commentPositions[i].start - start).match(/^\s*|\s*$/g)
-      if (matchReg.length === 1) continue
       const newStart = start + matchReg[0].length // 选中文本起始位置+算上\t\n后的位置
       const newEnd = commentPositions[i].start - matchReg[1].length // 首个<!--+去掉\t\n后的位置
       if (newStart !== newEnd) {
