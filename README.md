8d61056107dfd63d107cfd0482bdf541af40a853
zqylzcwcxy <347891134@qq.com>
2023-02-27 10:01:20 +0800

    :art:improve code structure or code format : 主分支仅保留文档

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # 🎉formatAndSave
-> HBuilderX插件-主要功能：嵌套注释、vue文件双分栏
+
+> HBuilderX 插件-主要功能：prettier 格式化、嵌套注释、vue 文件双分栏
 >
 > 插件市场 https://ext.dcloud.net.cn/plugin?id=7695
-> 
+>
 > 说明文档 https://zqy233.github.io/formatAndSave/
-
+>
+> 插件源码请查看 core 分支，master 分支用于 vitepress 文档
--- src/formatAndSave.js
+++
@@ -1,8 +0,0 @@
-const hx = require("hbuilderx")
-// 一键格式化和保存
-let formatAndSave = hx.commands.registerCommand("extension.formatAndSave", async () => {
-  hx.window.showInformationMessage('HELL')
-  await hx.commands.executeCommand("editor.action.format")
-  hx.commands.executeCommand("workbench.action.files.save")
-})
-module.exports = formatAndSave
